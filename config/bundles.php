bef0f647db1306ed2ce97e7d60295a849a328ab0
Timo Bakx <github@timobakx.dev>
2024-02-14 21:37:16 +0100

    Added monolog

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
