4f5970f8b03b7b945b968815c89c772583b9978e
Timo Bakx <github@timobakx.dev>
2024-03-02 15:40:53 +0100

    Removed unused use

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
